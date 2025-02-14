e0d1c9eb544ed7ad1d556283249e17e7804332fd
Catherine Meng <mcatherine1994@gmail.com>
2025-02-13 19:08:55 -0800

    feat: added side bar and summary layout

--- src/app.py
+++ src/app.py
@@ -1,53 +1,86 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
+# Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# Side bar for global filter
+sidebar = dbc.Col([
+    html.H3('Global Pharmaceutical Spend Dashboard'),
+    html.Br(),
+
+    html.H5('Country'),
+    dcc.Dropdown(),
+    html.Br(),
+
+    html.H5('Year'),
+    html.P('From', style={'margin-bottom': '0.375rem'}),
+    dcc.Dropdown(),
+    html.P('To', style={'margin-top': '0.375rem'}),
+    dcc.Dropdown(),
+    ],
+    md=3,
+    style={
+        'background-color': '#e6e6e6',
+        'padding': 15,  # Padding top,left,right,botoom
+        'padding-left': 30,
+        'width': '20vw',
+        'height': '100vh',  # vh = "viewport height" = 100% of the window height
+        'flex-direction': 'column',  # Allow for children to be aligned to bottom
+    }
+) 
+
+card_style = {'height': '100px'}
+
+# Summary status
+summary = dbc.Row(
+    [
+        html.H5('Summary Stats'),
+        dbc.Row([
+            dbc.Col(dbc.Card([
+                dbc.CardBody([
+                    html.H5("Avg % GDP"),
+                ])
+            ], style=card_style), width=3),  # Takes 4/12 columns
+
+            dbc.Col(dbc.Card([
+                dbc.CardBody([
+                    html.H5("Avg % of Health Spending"),
+                ])
+            ], style=card_style), width=3),
+
+            dbc.Col(dbc.Card([
+                dbc.CardBody([
+                    html.H5("Avg Spend per Capita (in USD)"),
+                ])
+            ], style=card_style), width=3),
+
+            dbc.Col(dbc.Card([
+                dbc.CardBody([
+                    html.H5("Total Spend (in USD m)"),
+                ])
+            ], style=card_style), width=3),
+        ])
+    ],
+    style={
+        'padding': 15,
+    }
+)
+
+# App layout
 app.layout = dbc.Container(
     [
-        dbc.Row(
-            dbc.Col(
-                html.H2("Global Pharmaceuitical Spendings"),
-                width=12
-            )
-        ),
-        
-        dbc.Row(
-            [   
-                dbc.Col(
-                    
-                    html.Div(["Global filters go here"]),
-                    width=4 
-                ),
-                
-                dbc.Col(
-                    [
-                        dbc.Row(
-                            [
-                                dbc.Col(html.Div("Local filters go here"), width=12)
-                            ]
-                        ),
-                        
-                        dbc.Row(
-                            [
-                                dbc.Col(html.Div("Graph 1"), width=6),
-                                dbc.Col(html.Div("Graph 2"), width=6),
-                            ]
-                        ),
-                        dbc.Row(
-                            [
-                                dbc.Col(html.Div("Graph 3"), width=6),
-                                dbc.Col(html.Div("Graph 4"), width=6),
-                            ]
-                        )
-                    ],
-                    width=8
-                ),
-            ]
-        )
+        dbc.Row([
+            sidebar,
+            dbc.Col(summary)
+        ]),
     ],
-    fluid=True,
+    fluid=True, # Expand to the full width of the window
+    style={
+        'padding': 0
+    }  
 )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
