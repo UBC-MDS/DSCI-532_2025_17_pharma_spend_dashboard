152af2132f41fc2638efc0d8a9e8ee0a13481a7c
MCatherine <Mcatherine1994@gmail.com>
2025-02-26 20:33:28 -0800

    feat: Added pie chart (#42)
    * feat: added year selection filter
    
    * feat: adjusted the page layout style, added metric selection title
    
    * feat: filtered data and added pie chart
    
    * fix: fixed the background color height of the sider bar
    
    * style: adjusted the style
    
    * fix: Use human readable title for pie chart

--- src/app.py
+++ src/app.py
@@ -8,31 +8,54 @@ import altair as alt
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-data = pd.read_csv("data/raw/data.csv")
+# Data preprocessing
+data = pd.read_csv('data/raw/data.csv')
+data = data.drop('FLAG_CODES', axis=1)
+
+locations = data['LOCATION'].unique()
+times = sorted(data['TIME'].unique()) # integer type
+min_year = data['TIME'].min()
+max_year = data['TIME'].max()
 
 # Side bar for global filter
-sidebar = dbc.Col([
-    html.H3('Global Pharmaceutical Spend Dashboard'),
-    html.Br(),
+sidebar = dbc.Col(
+    [
+        html.H3('Global Pharmaceutical Spend Dashboard'),
+        html.Br(),
 
-    html.H5('Country'),
-    dcc.Dropdown(id='country_select', options=[{'label': country, 'value': country} for country in data['LOCATION'].unique()], value=['CAN', 'USA', 'MEX'], multi=True),
-    html.Br(),
+        html.H5('Country'),
+        dcc.Dropdown(
+            id='country_select', 
+            options=[{'label': country, 'value': country} for country in locations], 
+            value=['CAN', 'USA', 'MEX'], 
+            multi=True
+        ),
+        html.Br(),
 
-    html.H5('Year'),
-    html.P('From', style={'margin-bottom': '0.375rem'}),
-    dcc.Dropdown(),
-    html.P('To', style={'margin-top': '0.375rem'}),
-    dcc.Dropdown(),
+        html.H5('Year'),
+        html.P('From', style={'marginBottom': '0.375rem'}),
+        dcc.Dropdown(
+            id='start_year_select',
+            options=[{'label': str(year), 'value': year} for year in times],
+            value=data['TIME'].min(),  # Default start by the minimal year
+            clearable=False
+        ),
+        html.P('To', style={'marginTop': '0.375rem'}),
+        dcc.Dropdown(
+            id='end_year_select',
+            options=[{'label': str(year), 'value': year} for year in times],
+            value=data['TIME'].max(),  # Default end by the maximum year
+            clearable=False
+        )
     ],
     md=3,
     style={
-        'background-color': '#e6e6e6',
+        'backgroundColor': '#e6e6e6',
         'padding': 15,  # Padding top,left,right,botoom
-        'padding-left': 30,
+        'paddingLeft': 30,
         'width': '20vw',
-        'height': '100vh',  # vh = "viewport height" = 100% of the window height
-        'flex-direction': 'column',  # Allow for children to be aligned to bottom
+        'minHeight': '100vh',  # vh = 'viewport height' = 100% of the window height
+        'flexDirection': 'column',  # Allow for children to be aligned to bottom
     }
 ) 
 
@@ -45,114 +68,150 @@ summary = dbc.Row(
         dbc.Row([
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H5("Avg % GDP"),
+                    html.H5('Avg % GDP'),
                 ])
             ], style=card_style), width=3),  # Takes 4/12 columns
 
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H5("Avg % of Health Spending"),
+                    html.H5('Avg % of Health Spending'),
                 ])
             ], style=card_style), width=3),
 
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H5("Avg Spend per Capita (in USD)"),
+                    html.H5('Avg Spend per Capita (in USD)'),
                 ])
             ], style=card_style), width=3),
 
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H5("Total Spend (in USD m)"),
+                    html.H5('Total Spend (in USD m)'),
                 ])
             ], style=card_style), width=3),
         ])
     ],
-    style={
-        'padding': 15,
-    }
+    style = {'paddingBottom': '1rem'}
 )
 
-#Metric Selector
-metric_selection = dcc.RadioItems(
-    id="spend_metric",
-    options=[
-        {"label": "Avg % GDP", "value": "PC_GDP"},
-        {"label": "% of Healthcare", "value": "PC_HEALTHXP"},
-        {"label": "USD Per Capita", "value": "USD_CAP"},
-        {"label": "Total Spend", "value": "TOTAL_SPEND"},
+# Metric Selector
+metric_selection = dbc.Row(
+    [
+        dbc.Col(html.H5('Spend Metrics'), width=2),
+        dbc.Col(
+            dcc.RadioItems(
+                id='spend_metric',
+                options=[
+                    {'label': 'Avg % GDP', 'value': 'PC_GDP'},
+                    {'label': '% of Healthcare', 'value': 'PC_HEALTHXP'},
+                    {'label': 'USD Per Capita', 'value': 'USD_CAP'},
+                    {'label': 'Total Spend', 'value': 'TOTAL_SPEND'},
+                ],
+                value='TOTAL_SPEND',  # Default selection
+                labelStyle={'display': 'inline-block', 'marginRight': '0.938rem'}
+            ),
+        )
     ],
-    value="TOTAL_SPEND",  # Default selection
-    labelStyle={'display': 'inline-block', 'margin-right': '15px'}
+    style = {'paddingBottom': '1rem'}
 )
 
 # Charts
-line_chart1 = dvc.Vega(id="chart1", spec={})
-line_chart2 = dvc.Vega(id="chart2", spec={})
-line_chart3 = dvc.Vega(id="chart3", spec={})
-line_chart4 = dvc.Vega(id="chart4", spec={})
+map_chart = dvc.Vega(id='map_chart', spec={})
+timeseries_chart = dvc.Vega(id='timeseries_chart', spec={})
+bar_chart = dvc.Vega(id='bar_chart', spec={})
+pie_chart = dvc.Vega(id='pie_chart', spec={})
 
 # App layout
 app.layout = dbc.Container(
     [
         dbc.Row([
-            dbc.Col(sidebar, width=3),
+            sidebar,
             dbc.Col([
                 summary,
-                dbc.Row(metric_selection),
+                metric_selection,
                 dbc.Row([
-                    dbc.Col(line_chart1, width=6),
-                    dbc.Col(line_chart2, width=6)
+                    dbc.Col(map_chart, width=6),
+                    dbc.Col(timeseries_chart, width=6)
                 ]),
                 dbc.Row([
-                    dbc.Col(line_chart3, width=6),
-                    dbc.Col(line_chart4, width=6)
+                    dbc.Col(bar_chart, width=6),
+                    dbc.Col(pie_chart, width=6)
                 ])
-            ], width=9)
+            ], style = {'paddingLeft': '1.25rem', 'paddingTop': '0.625rem'})
         ])
     ],
     fluid=True,
-    style={'padding': 0, 'margin': '10px'}
+    style={'padding': 0}
 )
+
+@app.callback(
+    Output('end_year_select', 'options'),
+    Output('end_year_select', 'value'),
+    Input('start_year_select', 'value'),
+    Input('end_year_select', 'value'),
+)
+def update_end_year_select_options(start, end):
+    # Ensure that the end year value cannot older than the start year
+    # Filter years that are greater than or equal to start_year
+    filtered_years = [year for year in times if year >= start]
+    options = [{'label': str(year), 'value': year} for year in filtered_years]
+
+    # Ensure the selected end year remains valid
+    default_end_year = end if end in filtered_years else filtered_years[0]
+
+    return options, default_end_year
 
 @callback(
-    Output('chart1', 'spec'),
-    Output('chart2', 'spec'),
-    Output('chart3', 'spec'),
-    Output('chart4', 'spec'),
+    Output('map_chart', 'spec'),
+    Output('timeseries_chart', 'spec'),
+    Output('bar_chart', 'spec'),
+    Output('pie_chart', 'spec'),
     Input('country_select', 'value'),
-    Input('spend_metric', 'value') 
-    #Add one more input that controls Year (Daria)
+    Input('start_year_select', 'value'),
+    Input('end_year_select', 'value'),
+    Input('spend_metric', 'value'),
+    Input('spend_metric', 'options')
 )
-def create_chart(country_select, spend_metric):
-    filtered_data = data[data['LOCATION'].isin(country_select)]
+def create_chart(country_select, start_year_select, end_year_select, spend_metric, spend_metric_options):
+    filtered_data = data[
+        data['LOCATION'].isin(country_select) &  # Filter by selected countries
+        data['TIME'].between(start_year_select, end_year_select)  # Filter TIME between years
+    ]
+    # Get average data group by country and year
+    avg_data = filtered_data.groupby('LOCATION')[spend_metric].mean().reset_index()
+    spend_metric_label = next(item['label'] for item in spend_metric_options if item['value'] == spend_metric)
 
     # Map Plot (Daria)
-    chart1 = alt.Chart(filtered_data).mark_point().encode(
-            x='TIME',
-            y=spend_metric,
-            color='LOCATION')
+    map_chart = alt.Chart(filtered_data).mark_point().encode(
+        x='TIME',
+        y=spend_metric,
+        color='LOCATION'
+    )
 
     # Time Series Chart (Jason)
-    chart2 = alt.Chart(filtered_data).mark_line().encode(
-            x='TIME',
-            y=spend_metric,
-            color='LOCATION')
-    
+    timeseries_chart = alt.Chart(filtered_data).mark_line().encode(
+        x='TIME',
+        y=spend_metric,
+        color='LOCATION'
+    )
+
     # Bar Chart (Celine)
-    chart3 = alt.Chart(filtered_data).mark_point().encode(
-            x='TIME',
-            y=spend_metric,
-            color='LOCATION'
-        )
+    bar_chart = alt.Chart(filtered_data).mark_point().encode(
+        x='TIME',
+        y=spend_metric,
+        color='LOCATION'
+    )
 
     # Pie Chart (Catherine)
-    chart4 = alt.Chart(filtered_data).mark_point().encode(
-            x='TIME',
-            y=spend_metric,
-            color='LOCATION'
-        )
-    return chart1.to_dict(), chart2.to_dict(), chart3.to_dict(), chart4.to_dict()
+    pie_chart = alt.Chart(avg_data).mark_arc().encode(
+        theta=alt.Theta(field=spend_metric, type='quantitative', stack=True),
+        color=alt.Color(field='LOCATION', type='nominal'),
+        tooltip=['LOCATION', spend_metric]
+    ).properties(
+        title=f'Average {spend_metric_label} by Country'
+    )
+
+    return map_chart.to_dict(), timeseries_chart.to_dict(), bar_chart.to_dict(), pie_chart.to_dict()
 
 if __name__ == '__main__':
     app.run()
