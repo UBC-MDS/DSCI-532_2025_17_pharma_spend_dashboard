1f0ce9c9bf9f2ee8ce8400d2545320fae463287a
dariakhv <112903408+dariakhv@users.noreply.github.com>
2025-02-27 17:19:20 -0800

    Added map chart to the dashboard (#43)
    * Added geopandas to environment.yml and requirement.txt
    
    * feat: Added map chart to the dashboard
    
    * feat: Added preprocessing data script to mege geospatial data
    
    * fix: fix the location dropdown to only include valid countries
    
    * fix: try to fix the import path
    
    * fix: try to fix the import path
    
    * fix: try to fix the import path
    
    * fix: try to fix the import path
    
    * fix: try to add vegafusion for large data
    
    * fix: fix vegafusion version
    
    * fix: fix the year selection options
    
    * fix: merge world countries in callback
    
    * cleanup: remove print information
    
    * feat: updated map with tooltips
    
    * fix: remove debug mode
    
    ---------
    
    Co-authored-by: Catherine Meng <mcatherine1994@gmail.com>

---
+++ src/preprocessing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import geopandas as gpd
+
+def preprocess(file_path):
+    data = pd.read_csv(file_path)
+    data = data.drop('FLAG_CODES', axis=1)
+    
+    # load geospatial data
+    url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+    world_countries = gpd.read_file(url)
+    world_countries = world_countries[
+        ["NAME", "SOV_A3", "CONTINENT", 'geometry']
+    ].rename(
+        columns=lambda x: x.lower()  # Lowercase column names
+    ).query(
+        'continent != "Antarctica"'
+    )
+    
+    replacement = {
+        'US1': 'USA', 
+        'NZ1': 'NZL', 
+        'FR1': 'FRA', 
+        'DN1': 'DNK', 
+        'IS1': 'ISR', 
+        'GB1': 'GBR', 
+        'NL1': 'NLD', 
+        'FI1': 'FIN', 
+        'AU1': 'AUS'
+    }
+    
+    world_countries = world_countries.replace(replacement)
+    world_countries = world_countries.rename(columns={'sov_a3': 'LOCATION'})
+    
+    print("Data Preprocessing Success!")
+
+    return data, world_countries
