c5a1d651ce532bf46a79c072edc41d521c1d773c
celinehabashy <21ch23@queensu.ca>
2025-03-05 22:05:05 -0800

    Fix: Updated summary card styling (#72)
    * Updated summary card styling
    
    * Added top padding to cards
    
    * Fixed preprocessing bug
    
    * Removed boxShadow from cards
    
    * fix: centered the card text
    
    ---------
    
    Co-authored-by: “Celine <celine.habashy@gmail.com>
    Co-authored-by: Jason Lee <jasonmlee.career@gmail.com>

--- src/app.py
+++ src/app.py
@@ -96,7 +96,14 @@ sidebar = dbc.Col(
     }
 )
 
-card_style = {'height': '125px'}
+
+card_style = {
+    "height": "125px",
+    "backgroundColor": "#e6e6e6",  # Light gray background 
+    "borderRadius": "10px",  # Rounded corners
+    "padding": "5px",
+    "paddingTop": "0.75rem"
+}
 
 # Summary status (Celine)
 summary = dcc.Loading(
@@ -104,37 +111,37 @@ summary = dcc.Loading(
         [
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H6("Avg % GDP"),
-                    html.H2(id="gdp-value", style={"fontWeight": "bold"}),  
+                    html.H6("Avg % GDP", style={"textAlign": "center"}),
+                    html.H2(id="gdp-value", style={"fontWeight": "bold", "textAlign": "center"}),  
                     html.P(id="gdp-growth", style={"color": "green", "fontSize": "14px"})  
                 ])
             ], style=card_style), md=3),
 
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H6("Avg % of Health Spending"),
-                    html.H2(id="health-value", style={"fontWeight": "bold"}),
+                    html.H6("Avg % of Health Spending", style={"textAlign": "center"}),
+                    html.H2(id="health-value", style={"fontWeight": "bold", "textAlign": "center"}),
                     html.P(id="health-growth", style={"color": "green", "fontSize": "14px"})
                 ])
             ], style=card_style), md=3),
 
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H6("Avg Spend per Capita (USD)"),
-                    html.H2(id="capita-value", style={"fontWeight": "bold"}),
+                    html.H6("Avg Spend per Capita (USD)", style={"textAlign": "center"}),
+                    html.H2(id="capita-value", style={"fontWeight": "bold", "textAlign": "center"}),
                     html.P(id="capita-growth", style={"color": "green", "fontSize": "14px"})
                 ])
             ], style=card_style), md=3),
 
             dbc.Col(dbc.Card([
                 dbc.CardBody([
-                    html.H6("Avg Total Spend (USD B)"),
-                    html.H2(id="total-value", style={"fontWeight": "bold"}),
+                    html.H6("Avg Total Spend (USD B)", style={"textAlign": "center"}),
+                    html.H2(id="total-value", style={"fontWeight": "bold", "textAlign": "center"}),
                     html.P(id="total-growth", style={"color": "green", "fontSize": "14px"})
                 ])
             ], style=card_style), md=3),
         ],
-        style={'paddingBottom': '1rem'}
+        style={'paddingBottom': '1rem', "paddingTop": "0.625rem"}
     ),
     type="cube", fullscreen=False, color="white"
 )
